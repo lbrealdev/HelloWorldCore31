91c9da3e6be46724d003d4a2d9786184f9a6e3ff
lbrealdev <lbrealdeveloper@gmail.com>
2024-12-19 11:01:57 +0100

    feat: add dotnet core project

---
+++ HelloWorldCore31/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldCore31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String hostname = Environment.MachineName;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
